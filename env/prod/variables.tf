768e04b77db3d1883dbd359950815de5a8894e87
Abhinavchandrangit <abhinavchandrasekhar2019@gmail.com>
2026-01-23 08:54:05 +0000

    vpc module and alb module and env\prod done succesfully

---
+++ env/prod/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zones"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public subnet CIDRs"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private subnet CIDRs"
+}
